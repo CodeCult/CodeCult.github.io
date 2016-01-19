6f867cdf45e3899d5c98350cbbd8d5973e9716d2
Elise Koster <koster.elise@gmail.com>
2016-01-19 16:42:05 +0100

    Added new page, fixed nav bar

--- projects/nav.js
+++ projects/nav.js
@@ -1,3 +1,4 @@
+console.log("LOADING");
 document.write("<nav class=\"navbar navbar-default navbar-fixed-top\">");
 document.write("        <div class=\"container\">");
 document.write("            <!-- Brand and toggle get grouped for better mobile display -->");
@@ -19,66 +20,134 @@ document.write("                        <a href=\"#page-top\"><\/a>");
 document.write("                    <\/li>");
 document.write("                    <li class=\"page-scroll\">");
 document.write("                        <div class=\"dropdown\">");
-document.write("                          <button class=\"btn btn-default dropdown-toggle\" type=\"button\" data-toggle=\"dropdown\" id=\"dropdownMenu1\"  aria-haspopup=\"true\" aria-expanded=\"true\">");
-document.write("                            Voor scholen");
-document.write("                            <span class=\"caret\"><\/span>");
-document.write("                          <\/button>");
-document.write("                          <ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenu1\">");
-document.write("                            <li><a href=\"basisschool.html\">Basisscholen<\/a><\/li>");
-document.write("                            <li><a href=\"middelbarescholen.html\">Middelbare Scholen<\/a>");
-document.write("                            <ul> ");
-document.write("                            <li><a href=\"middelbarescholen.html#workshops\">Algemeen<\/a><\/li>");
-document.write("                            <li><a href=\"middelbarescholen.html#vmbo\">Vernieuwing VMBO<\/a><\/li>");
-document.write("                            <\/ul>");
-document.write("                            <\/li>");
-document.write("                          <\/ul>");
+document.write("                          <a href=\"activiteiten.html\"><button class=\"btn btn-default\"  type=\"button\" id=\"dropdownMenu1\"  aria-haspopup=\"true\" aria-expanded=\"true\">");
+document.write("                            Activiteiten");
+document.write("                            <!--<span class=\"caret\"><\/span>-->");
+document.write("                          <\/button><\/a>");
+document.write("                        <\/div>");
+document.write("                    <\/li>");
+document.write("                    <li class=\"page-scroll\">");
+document.write("                        <div class=\"dropdown\">");
+document.write("                          <a href=\"scholen.html\"><button class=\"btn btn-default\"  type=\"button\" id=\"dropdownMenu1\"  aria-haspopup=\"true\" aria-expanded=\"true\">");
+document.write("                            Scholen");
+document.write("                            <!--<span class=\"caret\"><\/span>-->");
+document.write("                          <\/button><\/a>");
 document.write("                        <\/div>");
 document.write("                    <\/li>");
 document.write("                  <li class=\"page-scroll\">");
 document.write("                        <div class=\"dropdown\">");
-document.write("                          <button class=\"btn btn-default dropdown-toggle\" type=\"button\" data-toggle=\"dropdown\" id=\"dropdownMenu1\"  aria-haspopup=\"true\" aria-expanded=\"true\">");
-document.write("                            Voor jou");
-document.write("                            <span class=\"caret\"><\/span>");
-document.write("                          <\/button>");
-document.write("                          <ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenu1\">");
-document.write("                            <li><a href=\"kinderen.html\">Kinderen en Jongeren<\/a><\/li>");
-document.write("                                <ul>");
-document.write("                                    <li><a href=\"kinderen.html#kerst\">Kerstactiviteit<\/a><\/li>");
-document.write("                                    <li><a href=\"kinderen.html#programmeerclub\">Programmeerclub<\/a><\/li>");
-// document.write("                                    <li><a href=\"kinderen.html#codeherfst\">CodeHerfst<\/a><\/li>");
-document.write("                                <\/ul>");
-document.write("                            <li><a href=\"volwassenen.html\">Volwassenen<\/a><\/li>");
-document.write("                            <!--<li><a href=\"volwassenen.html\">Volwassenen<\/a><\/li>-->");
-document.write("                            <li><a href=\"resources.html\">Tips voor zelfstudie<\/a><\/li>");
-document.write("                          <\/ul>");
+document.write("                          <a href=\"partners.html\"><button class=\"btn btn-default\"  type=\"button\" id=\"dropdownMenu1\"  aria-haspopup=\"true\" aria-expanded=\"true\">");
+document.write("                            Partners");
+document.write("                            <!--<span class=\"caret\"><\/span>-->");
+document.write("                          <\/button><\/a>");
 document.write("                        <\/div>");
 document.write("                    <\/li>");
+document.write("");
+document.write("");
+document.write("      ");
 document.write("                  <li class=\"page-scroll\">");
 document.write("                        <div class=\"dropdown\">");
-document.write("                          <button class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\" type=\"button\" id=\"dropdownMenu1\" aria-haspopup=\"true\" aria-expanded=\"true\">");
-document.write("                            Over Ons");
-document.write("                            <span class=\"caret\"><\/span>");
-document.write("                          <\/button>");
-document.write("                          <ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenu1\">");
-document.write("                            <li><a href=\"over.html\">Over CodeCult<\/a><\/li>");
-document.write("                                    <li><a href=\"over.html#trainers1\">Het Team<\/a><\/li>");
-document.write("                            ");
-document.write("                          <\/ul>");
+document.write("                          <a href=\"overons.html\"><button class=\"btn btn-default\"  type=\"button\" id=\"dropdownMenu1\"  aria-haspopup=\"true\" aria-expanded=\"true\">");
+document.write("                            Over ons");
+document.write("                            <!--<span class=\"caret\"><\/span>-->");
+document.write("                          <\/button><\/a>");
 document.write("                        <\/div>");
 document.write("                    <\/li>");
-document.write("<li class=\"page-scroll\"><a href=\"sponsoring.html\" class=\"btn btn-md\" style=\"padding-top:0; padding-bottom:0; margin:5px; padding:10px 15px; display:flex;\" type=\"button\" id=\"dropdownMenu1\" >Partners <\/a><\/li>");
-document.write("                           ");
-
+document.write("");
+document.write("                    <li class=\"page-scroll\">");
+document.write("                        <div class=\"dropdown\">");
+document.write("                          <a href=\"contact.html\"><button class=\"btn btn-default\"  type=\"button\" id=\"dropdownMenu1\"  aria-haspopup=\"true\" aria-expanded=\"true\">");
+document.write("                            Contact");
+document.write("                            <!--<span class=\"caret\"><\/span>-->");
+document.write("                          <\/button><\/a>");
+document.write("                        <\/div>");
+document.write("                    <\/li>");
+document.write("                    ");
+document.write("                    ");
+document.write("");
 document.write("                <\/ul>");
 document.write("            <\/div>");
 document.write("            <!-- \/.navbar-collapse -->");
 document.write("        <\/div>");
 document.write("        <!-- \/.container-fluid -->");
-// document.write("    <\/nav>");
+document.write("    <\/nav>");
+console.log("THIS NAV");
+
 
 /* 
-Transformeer naar javascript op: http://www.accessify.com/tools-and-wizards/developer-tools/html-javascript-convertor/
+Originele code, gebruik om te bewerken en transformeer naar javascript op: http://www.accessify.com/tools-and-wizards/developer-tools/html-javascript-convertor/
+<nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="../index.html">CodeCult</a>
+            </div>
 
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <div class="dropdown">
+                          <a href="projects/activiteiten.html"><button class="btn btn-default"  type="button" id="dropdownMenu1"  aria-haspopup="true" aria-expanded="true">
+                            Activiteiten
+                            <!--<span class="caret"></span>-->
+                          </button></a>
+                        </div>
+                    </li>
+                    <li class="page-scroll">
+                        <div class="dropdown">
+                          <a href="scholen.html"><button class="btn btn-default"  type="button" id="dropdownMenu1"  aria-haspopup="true" aria-expanded="true">
+                            Scholen
+                            <!--<span class="caret"></span>-->
+                          </button></a>
+                        </div>
+                    </li>
+                  <li class="page-scroll">
+                        <div class="dropdown">
+                          <a href="partners.html"><button class="btn btn-default"  type="button" id="dropdownMenu1"  aria-haspopup="true" aria-expanded="true">
+                            Partners
+                            <!--<span class="caret"></span>-->
+                          </button></a>
+                        </div>
+                    </li>
+
+
+      
+                  <li class="page-scroll">
+                        <div class="dropdown">
+                          <a href="overons.html"><button class="btn btn-default"  type="button" id="dropdownMenu1"  aria-haspopup="true" aria-expanded="true">
+                            Over ons
+                            <!--<span class="caret"></span>-->
+                          </button></a>
+                        </div>
+                    </li>
+
+                    <li class="page-scroll">
+                        <div class="dropdown">
+                          <a href="contact.html"><button class="btn btn-default"  type="button" id="dropdownMenu1"  aria-haspopup="true" aria-expanded="true">
+                            Contact
+                            <!--<span class="caret"></span>-->
+                          </button></a>
+                        </div>
+                    </li>
+                    
+                    
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
 
 
 */
